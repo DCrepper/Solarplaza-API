0e5dd6a67bb9d431b95925388a025a2b934b4e8f
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-02-07 10:34:22 +0100

    Add route to trigger product import from API

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+use App\Jobs\ImportProductsJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('/admin');
 });
-
+Route::get('/update-products-from-api', function () {
+    ImportProductsJob::dispatch();
+    return redirect(null, 200);
+});
 require __DIR__ . '/auth.php';
